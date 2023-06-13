219384045347f9c86aa30212d8b3f5385dcd2f33
Motya26 <nbzhuravleva@gmail.com>
2023-06-13 13:17:15 +0100

    Update MyFirstProgram.java
    functions were added

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,23 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+	hello();
+
+  double len = 5;
+  System.out.println("Square with length " + len + " = " + area (len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("reqtangle with lengths " + a + " and " + b + " = " + area (a, b));
 	}
 
+  public static void hello() {
+    System.out.println("Hello, world!");
+  }
+public static double area (double l) {
+    return l * l;
+}
+  public static double area (double a, double b) {
+    return a * b;
+  }
 }
