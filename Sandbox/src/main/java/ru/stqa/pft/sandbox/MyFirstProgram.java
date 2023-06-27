6da39aba1db4d00d1e1083e16d335ae99e40f201
Motya26 <nbzhuravleva@gmail.com>
2023-06-27 12:41:31 +0100

    Square Test

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,21 +5,18 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 	hello();
 
-  double len = 5;
-  System.out.println("Square with length " + len + " = " + area (len));
+  System.out.println( 72855000 / 900);
 
-    double a = 4;
-    double b = 6;
-    System.out.println("reqtangle with lengths " + a + " and " + b + " = " + area (a, b));
+  Square s = new Square(6);
+  System.out.println("Square with length " + s.l + " = " + s.area());
+
+  Rectangle r = new Rectangle(4, 8);
+    System.out.println("rectangle with lengths " + r.a + " and " + r.b + " = " + r.area ());
+
 	}
 
   public static void hello() {
     System.out.println("Hello, world!");
   }
-public static double area (double l) {
-    return l * l;
-}
-  public static double area (double a, double b) {
-    return a * b;
-  }
+
 }
